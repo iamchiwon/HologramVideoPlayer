ac08a9b0ba25396c52f98f368b018d1fb8564736
Song Chiwon <iamchiwon@gmail.com>
2017-04-18 00:36:49 +0900

    비디오 재생, 비디오 촬영, 기능 완료

--- iOSPlayer/HologramPlayer/ViewControllers/VideoListViewController.swift
+++ iOSPlayer/HologramPlayer/ViewControllers/VideoListViewController.swift
@@ -8,110 +8,159 @@
 
 import UIKit
 import Photos
+import MobileCoreServices
 
 class VideoListViewController: UIViewController {
-  
+
   @IBOutlet weak var collectionView: UICollectionView!
-  
+  @IBOutlet weak var captureVideo: UIBarButtonItem!
+
   var videos = PHFetchResult<PHAsset>()
   var manager = PHImageManager.default()
-  
-  lazy var cellSize : CGSize = {
+
+  lazy var cellSize: CGSize = {
     let screenWidth = UIScreen.main.bounds.size.width
-    var cellSize : CGFloat = 150.0
-    
+    var cellSize: CGFloat = 150.0
+
     switch(screenWidth) {
-    case let w where w < 350 : //iphone 4S, SE
+    case let w where w < 350: //iphone 4S, SE
       cellSize = (screenWidth - 5 * 3) / 2
-      
-    case let w where w < 500 : //iphone 7, 7+
+
+    case let w where w < 500: //iphone 7, 7+
       cellSize = (screenWidth - 5 * 4) / 3
-      
-    case let w where w < 900 : //iPad  9.7"
+
+    case let w where w < 900: //iPad  9.7"
       cellSize = (screenWidth - 10 * 5) / 4
-      
-    default : //iPad 12.9"
+
+    default: //iPad 12.9"
       cellSize = (screenWidth - 10 * 6) / 5
     }
-    
+
     return CGSize(width: cellSize, height: cellSize)
   }()
-  
-  lazy var cellGap :CGFloat = {
+
+  lazy var cellGap: CGFloat = {
     let screenWidth = UIScreen.main.bounds.size.width
-    
+
     switch(screenWidth) {
-    case let w where w < 500 : //iphone 4S, SE, 7, 7+
+    case let w where w < 500: //iphone 4S, SE, 7, 7+
       return 5
-    default : //iPad  9.7", 12.9"
+    default: //iPad  9.7", 12.9"
       return 10
     }
   }()
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    navigationController?.isNavigationBarHidden = false
+    captureVideo.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     collectionView.delegate = self
   }
-  
+
+  override var prefersStatusBarHidden: Bool {
+    return false
+  }
+
   override func viewWillLayoutSubviews() {
     super.viewWillLayoutSubviews()
     collectionView.collectionViewLayout.invalidateLayout()
   }
-  
+
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    
+    navigationController?.isNavigationBarHidden = false
+    loadVideos()
+  }
+
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let identifier = segue.identifier, identifier == "PlayVideoSegue" {
+      if let indexPath = collectionView.indexPathsForSelectedItems?[0] {
+        let asset = videos.object(at: indexPath.row)
+        let destination = segue.destination as! VideoPlayerViewController
+        destination.videoAsset = asset
+      }
+    }
+  }
+
+  func loadVideos() {
     let options = PHFetchOptions()
     let sortByCreateDate = NSSortDescriptor(key: "creationDate", ascending: false)
     options.sortDescriptors = [sortByCreateDate]
     videos = PHAsset.fetchAssets(with: .video, options: options)
     collectionView.reloadData()
   }
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-  }
-  
-  @IBAction func onCaptureNewVideo(_ sender: Any) {
-    print("Capture new video")
-  }
-  
+
 }
 
-extension VideoListViewController : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-  
+extension VideoListViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return videos.count
   }
-  
+
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView .dequeueReusableCell(withReuseIdentifier: "VideoCell", for: indexPath);
-    
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "VideoCell", for: indexPath);
+
     if let imageView = cell.viewWithTag(1) as! UIImageView? {
       let asset = videos.object(at: indexPath.row)
       manager.requestImage(for: asset, targetSize: cellSize, contentMode: .aspectFill, options: nil, resultHandler: { (image, info) in
         imageView.image = image
       })
     }
-    
+
     return cell
   }
-  
+
   func collectionView(_ collectionView: UICollectionView,
                       layout collectionViewLayout: UICollectionViewLayout,
                       sizeForItemAt indexPath: IndexPath) -> CGSize {
     return cellSize
   }
-  
+
   func collectionView(_ collectionView: UICollectionView,
                       layout collectionViewLayout: UICollectionViewLayout,
                       minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
     return cellGap
   }
-  
+
   func collectionView(_ collectionView: UICollectionView,
                       layout collectionViewLayout: UICollectionViewLayout,
                       minimumLineSpacingForSectionAt section: Int) -> CGFloat {
     return cellGap
   }
+}
+
+extension VideoListViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+  @IBAction func onCaptureNewVideo(_ sender: Any) {
+    let imagePickerController = UIImagePickerController()
+    imagePickerController.sourceType = .camera
+    imagePickerController.allowsEditing = true
+    imagePickerController.mediaTypes = [kUTTypeMovie as String]
+    imagePickerController.showsCameraControls = true
+    imagePickerController.delegate = self
+
+    present(imagePickerController, animated: true)
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+    let urlOfVideo = info[UIImagePickerControllerMediaURL] as? NSURL
+    if let videoPath = urlOfVideo?.path {
+      if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(videoPath) {
+        UISaveVideoAtPathToSavedPhotosAlbum(videoPath, self, #selector(video(videoPath: didFinishSavingWithError: contextInfo:)), nil)
+      }
+    }
+
+    picker.dismiss(animated: true)
+  }
+
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true)
+  }
+
+  func video(videoPath: NSString, didFinishSavingWithError error: NSError?, contextInfo info: AnyObject) {
+    if error == nil {
+      loadVideos()
+    }
+  }
+
 }
--- iOSPlayer/HologramPlayer/ViewControllers/VideoPlayerViewController.swift
+++ iOSPlayer/HologramPlayer/ViewControllers/VideoPlayerViewController.swift
@@ -7,51 +7,139 @@
 //
 
 import UIKit
+import Photos
+import AVKit
+import AVFoundation
 
 class VideoPlayerViewController: UIViewController {
-  
+
   @IBOutlet weak var bottomControlBox: UIVisualEffectView!
   @IBOutlet weak var bottomBoxBottomConstrains: NSLayoutConstraint!
-  
+  @IBOutlet weak var player1: UIView!
+  @IBOutlet weak var player2: UIView!
+  @IBOutlet weak var player3: UIView!
+  @IBOutlet weak var player4: UIView!
+
+  var layer1: AVPlayerLayer!
+  var layer2: AVPlayerLayer!
+  var layer3: AVPlayerLayer!
+  var layer4: AVPlayerLayer!
+
+  var videoAsset: PHAsset!
+  var videoPlayer: AVPlayer! = nil
+
+  var standupVideo: Bool = true
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    navigationController?.isNavigationBarHidden = true
-    
+
+    flipVideo()
+
     let tapper = UITapGestureRecognizer(target: self, action: #selector(onTap))
     self.view.addGestureRecognizer(tapper)
+    bottomControlBox.isHidden = true
   }
-  
-  func onTap() {
-    showControlBox()
+
+  override var prefersStatusBarHidden: Bool {
+    return true
   }
-  
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.isNavigationBarHidden = true
+  }
+
   override func viewDidAppear(_ animated: Bool) {
-    showControlBox();
+    super.viewDidAppear(animated)
+    prepareVideo()
   }
-  
-  func showControlBox() {
-    bottomControlBox.isHidden = false
-    self.bottomBoxBottomConstrains.constant = 0
-    self.view.layoutIfNeeded()
-    
-    /*
-    UIView.animate(withDuration: 0.3,
-                   delay: 1.7,
-                   usingSpringWithDamping: 0.5,
-                   initialSpringVelocity: 0.7,
-                   options: UIViewAnimationOptions.curveEaseInOut,
-                   animations: {
-                    let height = self.bottomControlBox.bounds.size.height
-                    self.bottomBoxBottomConstrains.constant = -height
-                    self.view.layoutIfNeeded()
-    }) { (completion) in
-      self.bottomControlBox.isHidden = true
-    }*/
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    videoPlayer.pause()
   }
-  
+
+  func flipVideo() {
+    if standupVideo {
+      player1.transform = CGAffineTransform(scaleX: 1, y: -1)
+      player2.transform = CGAffineTransform(rotationAngle: 0.5 * .pi)
+      player3.transform = CGAffineTransform(scaleX: 1, y: -1).rotated(by: -0.5 * .pi)
+      player4.transform = CGAffineTransform(scaleX: 1, y: 1)
+    } else {
+      player1.transform = CGAffineTransform(scaleX: -1, y: 1)
+      player2.transform = CGAffineTransform(rotationAngle: -0.5 * .pi)
+      player3.transform = CGAffineTransform(scaleX: 1, y: -1).rotated(by: 0.5 * .pi)
+      player4.transform = CGAffineTransform(rotationAngle: 1.0 * .pi)
+    }
+  }
+
+  func prepareVideo() {
+    let options: PHVideoRequestOptions = PHVideoRequestOptions()
+    PHImageManager.default().requestAVAsset(forVideo: videoAsset, options: options) { (asset, mix, info) in
+      if let urlAsset = asset as? AVURLAsset {
+        let localVideoUrl = urlAsset.url
+        DispatchQueue.main.async {
+          self.videoPlayer = AVPlayer(url: localVideoUrl)
+          self.prepareVideoLayer()
+          self.playVideo()
+        }
+      }
+    }
+  }
+
+  func prepareVideoLayer() {
+    self.layer1 = AVPlayerLayer(player: self.videoPlayer)
+    self.layer2 = AVPlayerLayer(player: self.videoPlayer)
+    self.layer3 = AVPlayerLayer(player: self.videoPlayer)
+    self.layer4 = AVPlayerLayer(player: self.videoPlayer)
+
+    self.layer1.frame = self.player1.bounds
+    self.layer2.frame = self.player2.bounds
+    self.layer3.frame = self.player3.bounds
+    self.layer4.frame = self.player4.bounds
+
+    self.player1.layer.addSublayer(self.layer1)
+    self.player2.layer.addSublayer(self.layer2)
+    self.player3.layer.addSublayer(self.layer3)
+    self.player4.layer.addSublayer(self.layer4)
+  }
+
+  func playVideo() {
+    self.videoPlayer.play()
+
+    NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: self.videoPlayer.currentItem, queue: nil, using: { (_) in
+      DispatchQueue.main.async {
+        self.videoPlayer?.seek(to: kCMTimeZero)
+        self.videoPlayer?.play()
+      }
+    })
+  }
+
+  func onTap() {
+    bottomControlBox.isHidden = !bottomControlBox.isHidden
+  }
+
   @IBAction func onClose(_ sender: Any) {
     navigationController?.popViewController(animated: true)
   }
+
+  @IBAction func onFlip(_ sender: Any) {
+    standupVideo = !standupVideo
+    flipVideo()
+  }
+
+  @IBAction func onPause(_ sender: UIButton) {
+    if sender.title(for: .normal) == "Pause" {
+      videoPlayer.pause()
+      sender.setTitle("Play", for: .normal)
+    } else {
+      videoPlayer.play()
+      sender.setTitle("Pause", for: .normal)
+    }
+  }
   
+  @IBAction func onMute(_ sender: Any) {
+    videoPlayer.isMuted = !videoPlayer.isMuted
+  }
+
 }
